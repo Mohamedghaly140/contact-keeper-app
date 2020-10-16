fbd266c95954f7f1238709d0efa5796b673c3329
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-10-16 21:00:24 +0200

    Cleanup, create Navbar and About component & Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Components
+import Navbar from "./Components/Layout/Navbar/Navbar";
+import About from "./Components/Layout/Pages/About/About";
+import Home from "./Components/Layout/Pages/Home/Home";
+
+const App = () => {
+	return (
+		<Fragment>
+			<Navbar />
+			<div className="container">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+				</Switch>
+			</div>
+		</Fragment>
+	);
+};
 
 export default App;
