e4cf4932ba59ffd10d7c2a760811f1a41d355fc5
mohamedghaly140 <mohamedghaly140@gmail.com>
2020-10-16 22:35:08 +0200

    Contacts Context & Global State

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import ContactState from "./Context/Contact/contactState";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<ContactState>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</ContactState>,
 	document.getElementById("root")
 );
